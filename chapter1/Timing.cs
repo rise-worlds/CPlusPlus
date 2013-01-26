4fd05be6c819c70ddc9cbc96ecb6f4a6e34e351a
Rise <Rise.Worlds@Outlook.com>
2013-01-26 18:08:10 +0800

    add csharp learn

---
+++ chapter1/Timing.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace chapter1
+{
+	class Timing
+	{
+		TimeSpan startingTime;
+		TimeSpan duration;
+
+		public Timing()
+		{
+			startingTime = new TimeSpan(0);
+			duration = new TimeSpan(0);
+		}
+
+		public void stopTime()
+		{
+			duration = Process.GetCurrentProcess().Threads[0].UserProcessorTime.Subtract(startingTime);
+		}
+
+		public void startTime()
+		{
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
+			startingTime = Process.GetCurrentProcess().Threads[0].UserProcessorTime;
+		}
+
+		public TimeSpan Result()
+		{
+			return duration;
+		}
+	}
+}
---
+++ chapter1/chapter1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter1
+{
+	class chapter1
+	{
+		static int Main()
+		{
+			int[] nums = new int[100000];
+			BuildArray(nums);
+
+			Timing tObj = new Timing();
+			tObj.startTime();
+			DisplayNums(nums);
+			tObj.stopTime();
+
+			Console.WriteLine("time (.NET):" + tObj.Result().TotalSeconds);
+
+			return 0;
+		}
+
+		static void BuildArray(int[] arr)
+		{
+			for (int i = 0; i < 100000; i++)
+			{
+				arr[i] = i;
+			}
+		}
+
+		static void DisplayNums(int[] arr)
+		{
+			for (int i = 0; i <= arr.GetUpperBound(0); i++)
+			{
+				Console.Write(arr[i] + "  ");
+			}
+		}
+	}
+}
